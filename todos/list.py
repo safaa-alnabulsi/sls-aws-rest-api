4bfd72a2d4df55a2027e36572648138bb6b72aa6
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2020-03-08 18:23:58 +0100

    Implement the list action

--- todos/list.py
+++ todos/list.py
@@ -1,15 +1,20 @@
 import json
+import boto3
+import os
 
+dynamodb_table = os.environ['DYNAMODB_TABLE']
 
 def list(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    print('Inside the list function')
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table(dynamodb_table)
+    print(event)
+    result = table.scan()
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(result)
     }
 
     return response
