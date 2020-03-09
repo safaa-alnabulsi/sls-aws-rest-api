81b348c50a9d97a144bba37edcba415b0f26ac68
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2020-03-09 16:03:05 +0100

    Fix the get action by using query method

--- todos/get.py
+++ todos/get.py
@@ -1,20 +1,20 @@
 import json
 import boto3
 import os
+from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb_table = os.environ['DYNAMODB_TABLE']
+
 
 def get(event, context):
     print('Inside the get function')
     dynamodb = boto3.resource('dynamodb')
+
     table = dynamodb.Table(dynamodb_table)
-    print(event)
     id = event['pathParameters']['id']
-    print(id)
-    result = table.get_item(
-        Key={
-            'id': id
-        }
+
+    result = table.query(
+        KeyConditionExpression=Key('id').eq(id)
     )
 
     response = {
