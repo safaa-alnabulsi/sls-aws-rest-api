742f219e146390d1422333b4e3413d8c8cebda0e
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2020-03-09 18:20:54 +0100

    Update the delete action to delete the item by giving it only id

--- todos/delete.py
+++ todos/delete.py
@@ -1,23 +1,24 @@
 import json
 import boto3
 import os
+from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb_table = os.environ['DYNAMODB_TABLE']
 
+
 def delete(event, context):
+    print('Inside the delete function')
+
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(dynamodb_table)
 
-    item = json.loads(event['body'])
-    id = item['id']
-    content = item['todo']
+    id = event['pathParameters']['id']
 
-    result = table.delete_item(
-        Key={
-            "id": id,
-            "todo": content
-        }
-    )
+    result = table.query(KeyConditionExpression=Key('id').eq(id))
+    items = result['Items']
+
+    # delete the first element with the given hash
+    result = table.delete_item(Key=items[0])
 
     response = {
         "statusCode": 200,
